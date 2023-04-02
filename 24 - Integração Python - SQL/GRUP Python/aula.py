40c1df182a63cb3a2932958b6748fb3deda7795f
thiarly <thiarly.cavalcante@live.com>
2023-04-01 23:03:37 -0300

    função banco de dados de insert crud

--- 24 - Integração Python - SQL/GRUP Python/aula.py
+++ 24 - Integração Python - SQL/GRUP Python/aula.py
@@ -1,9 +1,33 @@
 import mysql.connector
 
+conexao = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='@Security0',
+    database='bdyoutube',
+)
+
+cursor = conexao.cursor()
 
 
+# CRUD
 
 # CREATE
+
+nome_produto = "todynho"
+valor = 3
+
+comando = f'INSERT INTO Vendas (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
+cursor.execute(comando)
+conexao.commit() # edita o banco de dados
+#resultado = cursor.fetchall() # ler o banco de dados
+
+
+
+cursor.close()
+conexao.close()
+
+
 
 
 # READ
