1fa70363e2cef7c0cae624f353f682c052c66a4b
thiarly <thiarly.cavalcante@live.com>
2023-04-02 22:06:29 -0300

    Aula 24, CRUD completo. Create, Update, Consult, Delete

--- 24 - Integração Python - SQL/GRUP Python/aula.py
+++ 24 - Integração Python - SQL/GRUP Python/aula.py
@@ -1,9 +1,9 @@
 import mysql.connector
 
 conexao = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='@Security0',
+    host='192.168.0.18',
+    user='lost',
+    password='@Children0',
     database='bdyoutube',
 )
 
@@ -13,27 +13,35 @@ cursor = conexao.cursor()
 # CRUD
 
 # CREATE
+# nome_produto = "pirulito"
+# valor = 2.50
 
-nome_produto = "todynho"
-valor = 3
+# comando = f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
+# cursor.execute(comando)
+# conexao.commit() # edita o banco de dados
 
-comando = f'INSERT INTO Vendas (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
+# READ
+# comando = f'SELECT * FROM vendas'
+# cursor.execute(comando)
+# resultado = cursor.fetchall() # ler o banco de dados
+# print(resultado)
+
+# UPDATE
+# nome_produto = "pirulito"
+# valor = 1
+
+# comando = f'UPDATE vendas SET valor = {valor} WHERE nome_produto = "{nome_produto}"'
+# cursor.execute(comando)
+# conexao.commit() # edita o banco de dados
+
+
+# DELETE
+nome_produto = "pirulito"
+
+comando = f'DELETE FROM vendas WHERE nome_produto = "{nome_produto}"'
 cursor.execute(comando)
 conexao.commit() # edita o banco de dados
-#resultado = cursor.fetchall() # ler o banco de dados
-
 
 
 cursor.close()
 conexao.close()
-
-
-
-
-# READ
-
-
-# UPDATE
-
-
-# DELETE
