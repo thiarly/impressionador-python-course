da97cf70a7874c2c81624aa022522a4994c0ba88
thiarly <thiarly.cavalcante@live.com>
2023-05-01 20:29:37 -0300

    update

--- 28 - Integração Python com APIs e JSON/API Firebase/aula.py
+++ 28 - Integração Python com APIs e JSON/API Firebase/aula.py
@@ -10,38 +10,38 @@
 import requests
 import json
 
-link = "seu_link_aqui"
+link = "https://apihashtag-default-rtdb.firebaseio.com"
 
-# Criar uma venda (POST)
-dados = {'cliente': 'alon', 'preco': 150, 'produto': 'teclado'}
+# # Criar uma venda (POST)
+dados = {'cliente': 'luca', 'preco': 150, 'produto': 'teclado'}
 requisicao = requests.post(f'{link}/Vendas/.json', data=json.dumps(dados))
 print(requisicao)
 print(requisicao.text)
 
-# Criar um novo produto (POST)
+# # Criar um novo produto (POST)
 dados = {'nome': 'teclado', 'preco': 150, 'quantidade': 80}
 requisicao = requests.post(f'{link}/Produtos/.json', data=json.dumps(dados))
 print(requisicao)
 print(requisicao.text)
 
 # Editar a venda (PATCH)
-dados = {'cliente': 'lira'}
-requisicao = requests.patch(f'{link}/Vendas/-MyJSm_N0S8KhCc3nAku/.json', data=json.dumps(dados))
+dados = {'cliente': 'clara'}
+requisicao = requests.patch(f'{link}/Vendas/-NUO0sOplb-yUSGGEc_q/.json', data=json.dumps(dados))
 print(requisicao)
 print(requisicao.text)
 
-# Pegar uma venda específico ou todas as vendas (GET)
+# # Pegar uma venda específico ou todas as vendas (GET)
 requisicao = requests.get(f'{link}/Vendas/.json')
 print(requisicao)
 dic_requisicao = requisicao.json()
-id_alon = None
+id_clara = None
 for id_venda in dic_requisicao:
     cliente = dic_requisicao[id_venda]['cliente']
-    if cliente == "alon":
+    if cliente == "clara":
         print(id_venda)
-        id_alon = id_venda
+        id_clara = id_venda
 
-# Deletar uma venda (DELETE)
-requisicao = requests.delete(f'{link}/Vendas/{id_alon}/.json')
+# # Deletar uma venda (DELETE)
+requisicao = requests.delete(f'{link}/Vendas/{id_clara}/.json')
 print(requisicao)
 print(requisicao.text)
