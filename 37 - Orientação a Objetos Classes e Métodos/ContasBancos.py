84007e207c36e02d72bf661c9cf6ca7c86efc540
thiarly <thiarly.cavalcante@live.com>
2023-05-05 16:51:07 -0300

    Update ContasBancos.py
    criando funções depositas e sacar dinheiro

--- 37 - Orientação a Objetos Classes e Métodos/ContasBancos.py
+++ 37 - Orientação a Objetos Classes e Métodos/ContasBancos.py
@@ -4,15 +4,35 @@ class ContaCorrente():
         self.cpf = cpf
         self.saldo = 0
 
+    def consultar_saldo(self):
+        print('O seu saldo atual é de R$ {:,.2f}'.format(self.saldo))
+
+    def depositar(self, valor):
+        self.saldo += valor
+        print('Deposito realizado {}'.format(valor))
+
+    def sacar_dinheiro(self, valor):
+        self.saldo -= valor
+        print('Saque realizado {}'.format(valor))
+
 
 
 # Programa
-
-
 conta_thiarly = ContaCorrente("Thiarly", "044.000.111-22")
+conta_thiarly.consultar_saldo()
 
-print(conta_thiarly.nome)
-print(conta_thiarly.cpf)
-print(conta_thiarly.saldo)
+
+# Depositando valor
+conta_thiarly.depositar(100000)
+conta_thiarly.consultar_saldo()
+
+# Sacando valor
+conta_thiarly.sacar_dinheiro(2500)
+conta_thiarly.consultar_saldo()
+
+
+# print(conta_thiarly.nome)
+# print(conta_thiarly.cpf)
+# print(conta_thiarly.saldo)
 
 
