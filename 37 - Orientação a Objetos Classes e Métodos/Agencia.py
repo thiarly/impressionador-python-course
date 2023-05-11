4596a2814e4db792caf22b3ad16f6c21eee163a6
thiarly <thiarly.cavalcante@live.com>
2023-05-11 17:14:18 -0300

    Update Agencia.py
    finalizando Classe agencia

--- 37 - Orientação a Objetos Classes e Métodos/Agencia.py
+++ 37 - Orientação a Objetos Classes e Métodos/Agencia.py
@@ -63,25 +63,36 @@ class AgenciaPremium(agencia):
         super().__init__(telefone, cnpj, numero=randint(1001, 9999))
         self.caixa = 5000000
 
+    def adicionar_cliente(self, nome, cpf, patrimonio):
+        if patrimonio > 1000000:
+            super().adicionar_cliente(nome, cpf, patrimonio)
+            print('Cliente adicionado com sucesso!')        
+        else:
+            print('O Cliente não tem patrimônio mínimo necessário para conta premium!')
 
 
+if __name__ == '__main__':
+
+    agencia_canudos_virtual = AgenciaVirtual('www.canudos_virtual.com.br', 41044104, 11111111111)
+    print('Agencia: {} Caixa: R$ {} Site: {}'.format(agencia_canudos_virtual.numero, agencia_canudos_virtual.caixa, agencia_canudos_virtual.site)) 
+    print('\n')
+
+    agencia_canudos_comum = AgenciaComum(3434343, 33333388888)
+    print('Agencia: {} Caixa R$ {}'.format(agencia_canudos_comum.numero, agencia_canudos_comum.caixa)) 
+    print('\n')
+
+    agencia_canudos_premium = AgenciaPremium(2434343, 23333388888)
+    print('Agencia: {} Caixa R$ {}'.format(agencia_canudos_premium.numero, agencia_canudos_premium.caixa)) 
 
 
-
-agencia_canudos_virtual = AgenciaVirtual('www.canudos_virtual.com.br', 41044104, 11111111111)
-print('Agencia: {} Caixa: R$ {} Site: {}'.format(agencia_canudos_virtual.numero, agencia_canudos_virtual.caixa, agencia_canudos_virtual.site)) 
-print('\n')
-
-agencia_canudos_comum = AgenciaComum(3434343, 33333388888)
-print('Agencia: {} Caixa R$ {}'.format(agencia_canudos_comum.numero, agencia_canudos_comum.caixa)) 
-print('\n')
-
-agencia_canudos_premium = AgenciaPremium(2434343, 23333388888)
-print('Agencia: {} Caixa R$ {}'.format(agencia_canudos_premium.numero, agencia_canudos_premium.caixa)) 
+    print(agencia_canudos_virtual.caixa)
+    print()
+    agencia_canudos_virtual.depositar_paypal(200)
+    print(agencia_canudos_virtual.caixa)
+    print(agencia_canudos_virtual.caixa_paypal)
 
 
-print(agencia_canudos_virtual.caixa)
-print()
-agencia_canudos_virtual.depositar_paypal(200)
-print(agencia_canudos_virtual.caixa)
-print(agencia_canudos_virtual.caixa_paypal)
+    agencia_canudos_premium.adicionar_cliente('Clara', 22222222, 11111000)
+    print(agencia_canudos_premium.clientes)
+
+
--- 37 - Orientação a Objetos Classes e Métodos/main.py
+++ 37 - Orientação a Objetos Classes e Métodos/main.py
@@ -1,20 +1,25 @@
 from ContasBancos import ContaCorrente, CartaoCredito
+from Agencia import AgenciaComum, AgenciaVirtual, AgenciaPremium
 
 
-
-# Programa
-conta_thiarly = ContaCorrente("Thiarly", "044.000.111-22", 8324, 156697)
-
-
-cartao_thiarly = CartaoCredito('Thiarly', conta_thiarly)
-
-print(cartao_thiarly.conta_corrente.conta )
-
-print(conta_thiarly.cartoes[0].numero)
-print(cartao_thiarly.validade)
-print(cartao_thiarly.cod_seguranca)
-
-cartao_thiarly.senha = '3111'
+# # Programa
+# conta_thiarly = ContaCorrente("Thiarly", "044.000.111-22", 8324, 156697)
 
 
-print(conta_thiarly.__dict__)
+# cartao_thiarly = CartaoCredito('Thiarly', conta_thiarly)
+
+# print(cartao_thiarly.conta_corrente.conta )
+
+# print(conta_thiarly.cartoes[0].numero)
+# print(cartao_thiarly.validade)
+# print(cartao_thiarly.cod_seguranca)
+
+# cartao_thiarly.senha = '3111'
+
+
+# print(conta_thiarly.__dict__)
+
+
+agencia_premium_cajazeiras = AgenciaPremium(8344332211, 7763535357272653)
+print(agencia_premium_cajazeiras.caixa)
+print(agencia_premium_cajazeiras.clientes)
