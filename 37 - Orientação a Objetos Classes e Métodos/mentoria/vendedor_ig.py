b778b8fd319e26e8ddd1dacb15ef6d7a839eb073
thiarly <thiarly.cavalcante@live.com>
2023-05-13 18:10:55 -0300

    def vendendor

--- 37 - Orientação a Objetos Classes e Métodos/mentoria/vendedor_ig.py
+++ 37 - Orientação a Objetos Classes e Métodos/mentoria/vendedor_ig.py
@@ -1,8 +1,46 @@
-class Vendedor():
-    vendas = 0
-    nome = ""
-    meta = 500
-    bonus = 0
+class ComissaoIntergard():
+    def __init__(self, nome, setor):
+        self.nome = nome
+        self.setor = setor
+        self.faturamento = 0
 
-vendedor_anderson = Vendedor()
-print(vendedor_anderson.meta)
+    def atualiza_faturamento(self, valor):
+        self.faturamento = valor
+
+    def calcular_comissao(self):
+        
+        if self.setor == "Vendedor":
+            comissao = self.faturamento * 0.01
+            comissao_formatada = "{:,.2f}".format(comissao)
+            comissao_formatada = comissao_formatada.replace(',', '.')
+            print('Comissão dos vendedores: R$ {}'.format(comissao_formatada))
+        
+        elif self.setor == "TI":
+            comissao_milhao = 600
+            calculo = self.faturamento // 1000000
+            comissao = calculo * comissao_milhao
+            comissao_formatada = "{:,.2f}".format(comissao)
+            comissao_formatada = comissao_formatada.replace(',', '.')
+            print('Comissão do setor de TI: R$ {}'.format(comissao_formatada))
+        
+        elif self.setor == "Assistência":
+            comissao_milhao = 500
+            calculo = self.faturamento // 1000000
+            comissao = calculo * comissao_milhao
+            comissao_formatada = "{:,.2f}".format(comissao)
+            comissao_formatada = comissao_formatada.replace(',', '.')
+            print('Comissão do setor da Assistência: R$ {}'.format(comissao_formatada))
+
+
+thiarly = ComissaoIntergard('thiarly', 'TI')
+thiarly.atualiza_faturamento(1000000)
+
+
+
+vendedores = ComissaoIntergard('Todos_Vendedores', 'Vendedor')
+vendedores.atualiza_faturamento(1000000)
+
+
+assistencia = ComissaoIntergard('Todos da Assistência', 'Assistência')
+assistencia.atualiza_faturamento(1000000)
+print (assistencia.calcular_comissao())
